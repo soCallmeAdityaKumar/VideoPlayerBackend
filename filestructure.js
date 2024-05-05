cda953c0ba2c9e7062c6a9bb0473790bea10dc07
Aditya kumar <adi54321kumar@gmail.com>
2024-05-06 00:45:34 +0530

    router , middlewares

---
+++ filestructure.js
@@ -0,0 +1,9 @@
+// ->src
+//   index.js(Db connects)    app.js(Cookies,urlencode)  constants(enum,Db_name)
+// ->Db
+// ->Models
+// ->Controllers
+// ->Routers
+// ->Middlewares
+// ->Utils
+// ->More(depends)
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 const asyncHandler=(fn)=>async(req,res,next)=>{
     try{
-        await fn(req,res,next)
+        return await fn(req,res,next)
     }catch(err){
         res.status(err.code||500).json({
             success:false,
