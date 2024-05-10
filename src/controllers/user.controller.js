f408bd8e3c16b3f56a5387f2dafaa6c03f1d6563
Aditya kumar <adi54321kumar@gmail.com>
2024-05-10 09:06:43 +0530

    user routes complete

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.models.js"
 import {upload_on_cloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
+import mongoose, { Types } from "mongoose";
 
 
 const registerUser=asyncHandler(async(req,res)=>{
@@ -337,4 +338,51 @@ const getUserChannelProfile=asyncHandler(async(req,res)=>{
 })
 
 
-export {registerUser,loginUser,loggedOutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage}
+const getWatchHistory=asyncHandler(async(req,res)=>{
+    const user= await User.aggregate([
+        {
+            $match:{
+                _id:new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res.status(200).json(new ApiResponse(200,user[0].watchHistory,"Watch History Fetched Successfully"))
+})
+
+
+export {registerUser,loginUser,loggedOutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,getWatchHistory}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import { loggedOutUser, loginUser, refreshAccessToken, registerUser } from '../controllers/user.controller.js'
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loggedOutUser, loginUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from '../controllers/user.controller.js'
 import {upload} from '../middlewares/multer.middlewares.js'
 import { verifyJWT } from '../middlewares/auth.middlewares.js'
 const router=Router()
@@ -23,5 +23,13 @@ router.route("/login").post(loginUser)
 //securedRoutes
 router.route("/logout").post(verifyJWT,loggedOutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/changePassword").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(getCurrentUser)
+router.route("/update_account").patch(verifyJWT,updateAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT,update.single("coverImage"),updateUserCoverImage)
+router.route("/channel/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/history").get(verifyJWT,getWatchHistory)
+
 
 export default router
